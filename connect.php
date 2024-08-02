cb7c213095a630e7267a04729bdc21bd77a287c8
Kryle8 <151596121+Kryle8@users.noreply.github.com>
2024-08-02 14:03:53 +0800

    Database Connection

---
+++ connect.php
@@ -0,0 +1,13 @@
+<?php
+// phpinfo();
+$serverName = "RYL";
+$connectionOptions = [
+    "Database"=>"yieldWerx_OJT2024",
+    "Uid"=>"",
+    "PWD"=>""
+];
+$conn = sqlsrv_connect($serverName, $connectionOptions);
+if($conn == false)
+    die( print_r( sqlsrv_errors(), true));
+else echo 'Connection Success';
+
