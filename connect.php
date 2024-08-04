cd7777d48cd0b1e97c42c48ab6c5582cb87d63b9
Kryle8 <151596121+Kryle8@users.noreply.github.com>
2024-08-04 19:04:33 +0800

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,11 +1,11 @@
 <?php
 // phpinfo();
-$serverName = "RYL";
-$connectionOptions = [
-    "Database"=>"yieldWerx_OJT2024",
-    "Uid"=>"",
-    "PWD"=>""
-];
-$conn = sqlsrv_connect($serverName, $connectionOptions);
-if($conn == false)
-    die( print_r( sqlsrv_errors(), true));
+// $serverName = "RYL";
+// $connectionOptions = [
+//     "Database"=>"yieldWerx_OJT2024",
+//     "Uid"=>"",
+//     "PWD"=>""
+// ];
+// $conn = sqlsrv_connect($serverName, $connectionOptions);
+// if($conn == false)
+//     die( print_r( sqlsrv_errors(), true));
