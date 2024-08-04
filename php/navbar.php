3c4e280d7dd4835064feefad30f158f8386a9b72
imfinegwenchana <162296361+imfinegwenchana@users.noreply.github.com>
2024-08-05 07:44:17 +0800

    Update navbar.php

--- php/navbar.php
+++ php/navbar.php
@@ -1,5 +1,5 @@
 <nav class="bg-white border-gray-200 dark:bg-gray-900 dark:border-gray-700">
-  <div class="max-w-screen-xl flex flex-wrap items-center justify-end mx-auto p-4">
+  <div class="max-w-screen-xl flex flex-wrap items-center justify-center mx-auto p-4">
     <a href="#" class="flex items-center space-x-3 rtl:space-x-reverse">
       <img src="../images/yieldwerx.png" class="h-12" alt="Yieldwerx Logo" />
     </a>
