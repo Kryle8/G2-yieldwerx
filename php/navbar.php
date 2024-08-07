3297afa2e83c04889f763ecaf62683b21fc8c729
imfinegwenchana <vonlandertrumpeta@gmail.com>
2024-08-07 08:39:27 +0800

    Update navbar.php

--- php/navbar.php
+++ php/navbar.php
@@ -1,4 +1,4 @@
-<nav class="bg-white border-gray-200 dark:bg-gray-900 dark:border-gray-700">
+<nav class="bg-white border-white bg-white border-white">
   <div class="max-w-screen-xl flex flex-wrap items-center justify-center mx-auto p-4">
     <a href="#" class="flex items-center space-x-3 rtl:space-x-reverse">
       <img src="../images/yieldwerx.png" class="h-12" alt="Yieldwerx Logo" />
